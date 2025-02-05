9a59fd44d5f7e2f432b28a391c155d3d2efec039
Crasheldark <ertiojulito@gmail.com>
2025-02-05 22:42:44 +0100

    Subiendo archivos

---
+++ src/EETTAREATEST/MyString1Test.java
@@ -0,0 +1,56 @@
+package EETTAREATEST;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import java.util.HashMap;
+import java.util.Map;
+
+import org.junit.jupiter.api.Test;
+
+import EETEjercicio.MyString1;
+
+class MyString1Test {
+
+	@Test
+	void testcontarpalabras (){
+	
+	assertEquals (2, MyString1.contarPalabras("hola mundo"));
+	
+	assertTrue (MyString1.contarPalabras("Me gusta jugar a la play")== 6);
+	
+	assertFalse (MyString1.contarPalabras("Me gusta jugar a la play")== 24);
+	
+		
+	
+	}
+	
+	@Test
+	void testcontarletrasdiferentes () {
+		
+		
+	assertEquals (8, MyString1.contarLetrasDiferentes("Hola Mundo"));
+	
+	assertTrue (MyString1.contarLetrasDiferentes("bonito dia")== 7);
+	
+	assertFalse (MyString1.contarLetrasDiferentes("que pereza")== 9);
+	
+	}
+	
+	@Test
+	void testcontarFrecuenciaPalabras () {
+		
+	
+	Map <String, Integer > botella = new HashMap<>();
+	botella.put("hola", 1);
+	botella.put("mundo", 2);
+	assertEquals (botella, MyString1.contarFrecuenciaPalabras("hola mundo mundo"));
+	
+	assertTrue (MyString1.contarFrecuenciaPalabras("hola mundo mundo").equals(botella));
+	
+	assertFalse (MyString1.contarFrecuenciaPalabras("hola mundo").equals(botella));
+	
+	
+		
+	}
+		
+}
